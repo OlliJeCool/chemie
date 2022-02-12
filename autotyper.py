2990efbfad7c97be5e817d827ddd563eed57ab37
Oliver Šulc <1001@student.itgymnazium.cz>
2022-02-12 23:13:15 +0100

    from formula to name

--- autotyper.py
+++
@@ -1,3 +0,0 @@
-import pyautogui as pg
-
-pg.write("Ahoj jak se máš lololol", 0.11)
---
+++ main.py
@@ -0,0 +1,103 @@
+import re
+
+
+
+def run():
+    proceed = True
+    bases = LoadOthers()
+    roots = LoadRoots()
+    endings = LoadEndings()
+    while proceed:
+        output = ""
+        _input = input("Enter a short or a full name")
+        if len(_input) <= 6:
+            count = []
+            temp = re.split('([A-Z0-9])', _input)
+            inputlist = list(filter(None, temp))
+            for i in range(0, len(inputlist)):
+                if inputlist[i].islower():
+                        inputlist[i-1] = inputlist[i-1]+inputlist[i]
+                        if i > 1:
+                            count.append(i-1)
+                        else:
+                            count.append(i)
+            for value in count:
+                inputlist.pop(value)
+
+            evalue = 0;
+
+            if len(inputlist) == 2: 
+                for base in bases:
+                    if base["short"] == inputlist[1]:
+                        output += base["iname"]+" "
+                        evalue = base["enum"]
+                for root in roots:
+                    if root["short"] == inputlist[0]:
+                        output += root["root"]
+                output += endings[abs(evalue)-1]
+                print(output)
+
+            if len(inputlist) == 3:
+                if inputlist[2].isnumeric():
+                    for base in bases:
+                        if base["short"] == inputlist[1]:
+                            output += base["iname"]+" "
+                            evalue = base["enum"]*int(inputlist[2])
+                    for root in roots:
+                        if root["short"] == inputlist[0]:
+                            output += root["root"]
+                    output += endings[abs(evalue)-1]
+                    print(output)
+                if inputlist[1].isnumeric():
+                    for base in bases:
+                        if base["short"] == inputlist[2]:
+                            output += base["iname"]+" "
+                            evalue = base["enum"]
+                    for root in roots:
+                        if root["short"] == inputlist[0]:
+                            output += root["root"]
+                    output += endings[int(abs((evalue))/int(inputlist[1]))-1]
+                    print(output)
+
+            if len(inputlist) == 4:
+                for base in bases:
+                    if base["short"] == inputlist[2]:
+                        output += base["iname"]+" "
+                        evalue = base["enum"]*int(inputlist[3])
+                for root in roots:
+                    if root["short"] == inputlist[0]:
+                        output += root["root"]
+                output += endings[int(abs(evalue/int(inputlist[1])))-1]
+                print(output)
+        if len(_input) > 6:
+            temp = _input.split()
+
+
+
+def LoadRoots():
+    elements = []
+    with open("./roots.txt", "r", encoding="utf-8") as f:
+        lines = [line.rstrip('\n') for line in f]
+        for line in lines:
+            temp = line.split(';')            
+            elements.append({"num" : int(temp[0]), "short" : temp[1], "root" : temp[2]})
+    return elements
+ 
+def LoadOthers():
+    bases = []
+    with open("./placeholder.txt", "r", encoding="utf-8") as t:
+        lines = [line.rstrip('\n') for line in t]
+        for elements in lines:
+            temp = elements.split(';')
+            bases.append({"short" : temp[0], "name":temp[1], "enum": int(temp[2]), "iname" : temp[3]})
+    return bases
+
+def LoadEndings():
+    endings = []
+    with open("./koncovky.txt", "r" , encoding="utf-8") as t:
+        temp = t.readline()
+    endings = temp.split(";")
+    return endings
+
+
+run()
